c84460b9059b443a918ea41f678ea97d268ad004
uraura <t@urau.la>
2019-01-07 00:32:11 +0900

    add sub view

---
+++ sample/SubViewController.swift
@@ -0,0 +1,26 @@
+//
+//  SubViewController.swift
+//  sample
+//
+//  Created by uraura on 2019/01/07.
+//  Copyright © 2019 uraura. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class SubViewController: UIViewController{
+
+    @IBOutlet var label: UILabel!
+    var r: NSInteger!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        label.text = String(r)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+}
--- sample/ViewController.swift
+++ sample/ViewController.swift
@@ -8,24 +8,57 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
     
-    @IBOutlet var label: UILabel!
-    @IBOutlet var button: UIButton!
+     @IBOutlet var table: UITableView!
     
-    var count = 0
+    var r = 0
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        
-        label.text = "Hello World!"
-        button.setTitle("xxx", for: UIControl.State.normal)
     }
 
-    @IBAction func buttonTapped(_sender: Any) {
-        count += 1
-        label.text = count % 2 == 0 ? "even" : "odd"
+    func tableView(_ table: UITableView,
+                   numberOfRowsInSection section: Int) -> Int {
+        // TODO ファイルの個数
+        return 5
+    }
+    //各セルの要素を設定する
+    func tableView(_ table: UITableView,
+                   cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+
+        // tableCell の ID で UITableViewCell のインスタンスを生成
+        let cell = table.dequeueReusableCell(withIdentifier: "tableCell",
+                                             for: indexPath)
+
+        // Tag番号 ２ で UILabel インスタンスの生成
+        let label1 = cell.viewWithTag(2) as! UILabel
+        label1.text = "No." + String(indexPath.row + 1)
+
+        return cell
+    }
+    // Cell の高さを１２０にする
+    func tableView(_ table: UITableView,
+                   heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 48
+    }
+
+    // Cell が選択された場合
+    func tableView(_ table: UITableView,didSelectRowAt indexPath: IndexPath) {
+        //
+        r = indexPath.row + 1
+        // SubViewController へ遷移するために Segue を呼び出す
+        performSegue(withIdentifier: "toSubViewController",sender: nil)
+    }
+
+    // Segue 準備
+    override func prepare(for segue: UIStoryboardSegue, sender: Any!) {
+        if (segue.identifier == "toSubViewController") {
+            let subVC: SubViewController = (segue.destination as? SubViewController)!
+            // SubViewController のrを設定
+            subVC.r = r
+        }
     }
 }
 
